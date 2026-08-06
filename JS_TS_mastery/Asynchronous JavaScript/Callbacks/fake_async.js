7488fe0120d0f903aacda9db45c3967eb8a71c2a
Shahruk <mdshahruk2020@gmail.com>
2026-08-06 09:47:38 +0600

    practice mini example for callbackds hell

---
+++ JS_TS_mastery/Asynchronous JavaScript/Callbacks/fake_async.js
@@ -0,0 +1,16 @@
+setTimeout(function(){
+    console.log("downloading....");
+    setTimeout(function(){
+    console.log("processing...");
+    setTimeout(function(){
+        console.log("saving...");
+        setTimeout(function(){
+        console.log("done");
+        
+        },1000);
+        
+    },1000);
+    
+    },1000);
+    
+},1000);
