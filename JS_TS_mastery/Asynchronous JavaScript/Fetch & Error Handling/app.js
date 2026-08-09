71056089e8d7034702455caa8dcf7b5cadd1a0ed
Shahruk <mdshahruk2020@gmail.com>
2026-08-09 11:58:03 +0600

    topic: fetch and error handling - make a project githhub user find - output name, followers, public repo, location - also error handling wiht status message

---
+++ JS_TS_mastery/Asynchronous JavaScript/Fetch & Error Handling/app.js
@@ -0,0 +1,28 @@
+async function githubUserFinder(){
+    try {
+        const res = await fetch('https://api.github.com/users/octocat');
+        
+        if (!res.ok){
+            
+            const statusMessages = {
+                404: 'User not found',
+                403: 'Rate limit exceeded.',
+                500: 'GitHub server error.'
+            };
+            
+            const message = statusMessages[res.status] || `Error ${res.status}: Something went wrong`;
+            throw new Error(message);
+        }
+        
+        const data = await res.json();
+        console.log(`Name: ${data.name}`);
+        console.log(`Followers: ${data.followers}`);
+        console.log(`Public repos: ${data.public_repos}`);
+        console.log(`Location: ${data.location}`);
+        
+    } catch(error) {
+        console.log('Error:', error.message);
+    }
+}
+
+githubUserFinder();
